c039a40c2808f04acf98867ab4c03e532f9850c5
youssef shafek <youssefmsa616@gmail.com>
2024-07-16 20:41:30 +0300

    getting data from json server
    Signed-off-by: youssef shafek <youssefmsa616@gmail.com>

--- js/main.js
+++ js/main.js
@@ -8,31 +8,13 @@ document.addEventListener("DOMContentLoaded", () => {
     let selectedCustomer = null;
     let displayedchart = null;
 
-    function fetchData() {
-        customers = [
-            { "id": 1, "name": "Ahmed Ali" },
-            { "id": 2, "name": "Aya Elsayed" },
-            { "id": 3, "name": "Mina Adel" },
-            { "id": 4, "name": "Sarah Reda" },
-            { "id": 5, "name": "Mohamed Sayed" }
-        ];
-        transactions = [
-            { "id": 1, "customer_id": 1, "date": "2022-01-01", "amount": 1000 },
-            { "id": 2, "customer_id": 1, "date": "2022-01-02", "amount": 2000 },
-            { "id": 3, "customer_id": 2, "date": "2022-01-01", "amount": 550 },
-            { "id": 4, "customer_id": 3, "date": "2022-01-01", "amount": 500 },
-            { "id": 5, "customer_id": 2, "date": "2022-01-02", "amount": 1300 },
-            { "id": 6, "customer_id": 4, "date": "2022-01-01", "amount": 750 },
-            { "id": 7, "customer_id": 3, "date": "2022-01-02", "amount": 1250 },
-            { "id": 8, "customer_id": 5, "date": "2022-01-01", "amount": 2500 },
-            { "id": 9, "customer_id": 5, "date": "2022-01-02", "amount": 875 },
-            { "id": 10, "customer_id": 1, "date": "2022-01-02", "amount": 500 }
-        ];
+    async function fetchData() {
+        let customersresp = await fetch('http://localhost:3000/customers')
+        customers = await customersresp.json()
+        let transactionsresp = await fetch('http://localhost:3000/transactions')
+        transactions = await transactionsresp.json()
         displayTable();
     }
-
-
-
     fetchData();
     function displayTable() {
         customerTableBody.innerHTML = "";
@@ -41,9 +23,10 @@ document.addEventListener("DOMContentLoaded", () => {
         );
         const filteredTransactions = transactions.filter(transaction => transaction.amount >= amountFilter.value);
 
+
         filteredTransactions.forEach(transaction => {
             filteredCustomers.forEach(customer => {
-                if (transaction.customer_id === customer.id) {
+                if (transaction.customer_id == customer.id) {
                     const row = document.createElement("tr");
                     row.innerHTML = `
                         <td>${transaction.id}</td>
@@ -64,7 +47,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
     function displayChart() {
-        const customerTransactions = transactions.filter(t => t.customer_id === selectedCustomer.id);
+        const customerTransactions = transactions.filter(t => t.customer_id == selectedCustomer.id);
         const chartData = {
             labels: customerTransactions.map(t => t.date),
             datasets: [{
